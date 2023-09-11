4a7d2c339b4e031d2c279f883acfdda4d79e5adf
MucheruM <petermucheru420@gmail.com>
2023-09-11 08:34:32 -0700

    A program to print puzzle analogy message using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print "Programming is like building a multilingual puzzle"
+ * using the puts function
  *
  * Return: Always 0 (Success)
  */

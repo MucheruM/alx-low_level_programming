d2df0480ffb0ba6771a692d4fcac912fe9e52aa2
MucheruM <petermucheru420@gmail.com>
2023-09-14 18:12:44 -0700

    Fizz Buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,28 +10,18 @@ int main(void)
 	int a;
 
 	for (a = 1; a <= 100; a++)
-	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
+{
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		}
-		else if (a % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (a % 3 == 0)
-		{
+		else if (i % 3 == 0)
 			printf("Fizz");
-		}
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%i", a);
-		}
-
-		if (a < 100)
-		{
+			printf("%a", i);
+		if (i < 100)
 			printf(" ");
-		}
 	}
+	printf("\n");
 	return (0);
 }

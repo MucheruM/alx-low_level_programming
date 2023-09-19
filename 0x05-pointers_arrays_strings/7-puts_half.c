f02f30127d50b4d6da55702a1799fd8cd9e2b2d9
MucheruM <petermucheru420@gmail.com>
2023-09-18 19:51:10 -0700

    m

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half the string
+ * @str: char to check
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, a;
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	if (len + 1 % 2 != '0')
+		a = (len - 1) / 2;
+	else
+		a = (len / 2);
+	a++;
+
+	for (len = a; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
+}

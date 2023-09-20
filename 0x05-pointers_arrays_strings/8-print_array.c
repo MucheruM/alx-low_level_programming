c4c7e9ce2d930f5907cc81f16106b45bd961497f
MucheruM <petermucheru420@gmail.com>
2023-09-19 18:41:03 -0700

    Print n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
+		if (i != n - 1) /* The string n - 1 means last char in array */
 			printf("%d, ", a[i]);
 		else
-			printf("%d", a[i]);
+			printf("%d", a[i]); /* Last char no need for "," and " " */
 	}
 	printf("\n");
 }

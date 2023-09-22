1d362fbe3748ef272753c48100515730897161d9
MucheruM <petermucheru420@gmail.com>
2023-09-21 17:17:53 -0700

    Print and integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,17 @@
  */
 void print_number(int n)
 {
-	int m;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
 
-	m = n;
-	if (m / 10)
+	if (n / 10)
 	{
-		print_number(m / 10);
+		print_number(n / 10);
 	}
-	_putchar(m % 10 + '0');
+
+	_putchar(n % 10 + '0');
 }

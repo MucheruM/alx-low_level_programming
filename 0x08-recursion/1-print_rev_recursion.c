fd05c6a38f8019bf055ba4696bf05703c5035aa4
MucheruM <petermucheru420@gmail.com>
2023-09-27 09:27:09 -0700

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -12,6 +12,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1); /* logic is recursive calls hapen*/
 		_putchar(*s); /*printing from the last char to first*/
 	}
-	else
-		_putchar('\n');
 }

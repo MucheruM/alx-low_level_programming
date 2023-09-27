c7d923e9d725b262b8556d43f602653548572892
MucheruM <petermucheru420@gmail.com>
2023-09-27 09:25:05 -0700

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_rev_recursion - print string in reverse
+ * @s: the pointer to the string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1); /* logic is recursive calls hapen*/
+		_putchar(*s); /*printing from the last char to first*/
+	}
+	else
+		_putchar('\n');
+}

d33cf9c7bf2569037689285a394cb9e85ce5a6f6
MucheruM <petermucheru420@gmail.com>
2023-09-27 05:07:51 -0700

    Print a string followed by '\n'

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string '\n'
+ * @s: pointer to the address of the string to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}

88c9bf074126ce7dff4a57781521e640aa156b9c
MucheruM <petermucheru420@gmail.com>
2023-09-29 13:15:11 -0700

    Program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all arguments passed,
+ * to the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv)
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", arg[count]);
+	return (0);
+}

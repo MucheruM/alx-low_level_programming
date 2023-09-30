e5e16e29f8b8fdf5d77d2c1cb042e24590c59b8a
MucheruM <petermucheru420@gmail.com>
2023-09-30 12:09:19 -0700

    Modified program that prints its name follewed by \n

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,10 @@
  * @argc: argument count - # of commd line arg passed to program
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	(void)argc;
 
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }

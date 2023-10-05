4ab7ec9d7f33ebdc4d7254f652373afaef91e758
MucheruM <petermucheru420@gmail.com>
2023-10-05 07:12:34 -0700

    Allocated memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  * *malloc_checked - function that allocated mem using malloc
  * @b: the number of bytes to be allocated by the malloc funct
- * Return: A pointer to the allocated mem, else
+ * Return: A pointer of ANYTYPE to the allocated mem, else
  * 98 if malloc fails.
  */
 void *malloc_checked(unsigned int b)

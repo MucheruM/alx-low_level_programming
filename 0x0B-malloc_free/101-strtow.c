639b7e5b8cbc3bdd98d6ca9dde459221e45f4a79
MucheruM <petermucheru420@gmail.com>
2023-10-06 04:43:08 -0700

    A function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,72 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * count_word - helper function to count the # of words in str
+ * count_words - helper function to count the number of words in a string
  * @str: string to evaluate
+ *
  * Return: number of words
  */
-int count_word(char *str)
+int count_words(char *str)
 {
-	int flag = 0;
-	int w = 0, i;
+	int s, i, w;
+
+	s = 0;
+	w = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ')
-			flag = 0;
-		else if (flag == 0)
+			s = 0;
+		else if (s == 0)
 		{
-			flag = 1;
+			s = 1;
 			w++;
 		}
 	}
+
 	return (w);
 }
 
 /**
- * **strtow - string tokenize function
- * @str: recieves input as a pointer to a string
- * Return: a pointer to a pointer of a character
+ * strtow - split a str into words
+ * @str: str to be split
+ * Return: pointer to the array of splitted words
  */
 char **strtow(char *str)
 {
-	char **words; /* stor words from str */
-	char *word, *str_cpy, *saveptr; /* hold words,cpy, ptr for strtok_r */
-	int count; /* count no of wrds in str */
-	unsigned int i, j; /* use for loop counters */
+	char **word, *tmp_w;
+	int str_i, word_i = 0, str_len = 0, word_c, char_c = 0, word_s, word_e;
 
-	if (str == NULL || *str == '\0')
-		return (NULL); /* str properly init in mem & str ! '\0' */
-	str_cpy = strdup(str); /* preserve org str and mem managemnt */
-	if (str_cpy == NULL)
+	while (*(str + str_len))
+		str_len++;
+	word_c = count_words(str);
+	if (word_c == 0)
 		return (NULL);
-	count = count_word(str_cpy); /* count word fnction */
 
-	words = malloc((count + 1) * sizeof(char *));
-	if (words == NULL) /* chck if strng is init in mem */
-	{ /* create mem of strn + 1 byte for the '\0' */
-		free(str_cpy);
+	word = malloc(sizeof(char *) * (word_c + 1));
+	if (word == NULL)
 		return (NULL);
-	} /* reentrant */
-	word = strtok_r(str_cpy, " ", &saveptr); /* str to token (word) */
-	for (i = 0; word != NULL; i++) /* chck for any rem tokens */
+
+	for (str_i = 0; str_i <= str_len; str_i++)
 	{
-		words[i] = strdup(word); /* mak mem with loc 'i' */
-		if (words[i] == NULL)
+		if (str[str_i] == ' ' || str[str_i] == '\0')
 		{
-			for (j = 0; j < i; j++) /* iterate str */
-				free(words[j]); /* free their mem */
-			free(words); /* free mem incase depletes */
-			free(str_cpy);
-			return (NULL);
+			if (char_c)
+			{
+				word_e = str_i;
+				tmp_w = malloc(sizeof(char) * (char_c + 1));
+				if (tmp_w == NULL)
+					return (NULL);
+				while (word_s < word_e)
+					*tmp_w++ = str[word_s++];
+				*tmp_w = '\0';
+				word[word_i] = tmp_w - char_c;
+				word_i++;
+				char_c = 0;
+			}
 		}
-		word = strtok_r(NULL, " ", &saveptr);
+		else if (char_c++ == 0)
+			word_s = str_i;
 	}
-	words[i] = NULL;
-	free(str_cpy);
-	return (words);
+
+	word[word_i] = NULL;
+
+	return (word);
 }
 
 /**

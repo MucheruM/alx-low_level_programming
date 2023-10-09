c179e4fa947a157865b09c64b8aa29eaa7a07756
MucheruM <petermucheru420@gmail.com>
2023-10-09 08:38:36 -0700

    Print file location using __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
 * main - print name where file is compiled from using _FILE_
@@ -7,7 +6,7 @@
 */
 int main(void)
 {
-	printf(" %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }

07e9fde718f4e088446c2a70106130963f0335ea
MucheruM <petermucheru420@gmail.com>
2023-10-10 00:46:26 -0700

    Define a struct with the given element

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Define struct with the given elements
  * @name: First member
@@ -10,3 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */

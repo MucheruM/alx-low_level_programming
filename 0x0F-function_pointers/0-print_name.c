b649d746d561b6d2b2aff2429ad78a61a5dc3e89
MucheruM <petermucheru420@gmail.com>
2023-10-11 04:10:58 -0700

    Function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
 /**
  * print_name - Function we are pointing to using f
  * @name: Pointer to an array of characters, a string of names
+ * @f: Pointer to the print_name function
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))

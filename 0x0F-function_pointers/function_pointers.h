09476c2d61fcfc81f03ad963a49ac4577739a8b0
MucheruM <petermucheru420@gmail.com>
2023-10-11 19:47:30 -0700

    Add function prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,6 +7,10 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+
 int (*get_op_func(char *s))(int, int);
 
 #endif

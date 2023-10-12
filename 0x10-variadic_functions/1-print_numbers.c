578a390620c00ff4afe2ba66a277c89567509a06
MucheruM <petermucheru420@gmail.com>
2023-10-12 14:58:17 -0700

    Add prototype functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,11 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num_list = va_arg(num, unsigned int);
 		printf("%d", num_list);
-		if (separator == NULL)
-		{
-			;
-		}
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");

d39f5c5b2099edfebf803f582aefa80706bee43e
MucheruM <petermucheru420@gmail.com>
2023-10-24 11:28:33 -0700

    Write a function that prints all the elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num_nodes++;
 		h = h->next; /*This is pointing to the next node*/
+		num_nodes++;
 	}
 	return (num_nodes);
 }

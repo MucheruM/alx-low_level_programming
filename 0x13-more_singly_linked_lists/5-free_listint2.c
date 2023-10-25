21062ba08fb223c01b89c427af14a95d4f8151ff
MucheruM <petermucheru420@gmail.com>
2023-10-24 18:45:31 -0700

    Write a function that frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,10 +12,12 @@ void free_listint2(listint_t **head)
 
 	if (!head)
 		return;
-	while (head)
+	while (*head)
 	{
 		next = (*head)->next;
 		free(*head);
 		*head = next;
 	}
+
+	*head = NULL;
 }

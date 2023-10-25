d8cb75f657d233150483b0d54a0eb5108593f384
MucheruM <petermucheru420@gmail.com>
2023-10-24 18:33:42 -0700

    Write a function that frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: first element of the list
+ * Return: NULL, if error
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while(head)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}

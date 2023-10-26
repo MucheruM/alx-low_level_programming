3601251d62b574e09aaf6e92f57bb6a6abfab0e8
MucheruM <petermucheru420@gmail.com>
2023-10-26 06:38:35 -0700

    Add prototype functions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
  * Description: Return: the converted number, or 0 if,
  * there is one or more chars in the string b that is not 0 or 1,
  * b is NULL
-
+ *
  * Return: An unsigned value
  */
 unsigned int binary_to_uint(const char *b)

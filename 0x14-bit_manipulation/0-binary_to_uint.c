4dc308860f451e8d1962397c337a783969220e62
MucheruM <petermucheru420@gmail.com>
2023-10-26 06:40:33 -0700

    Write a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  * binary_to_uint - converts binary num to unsigned int
  * @b: pointer to the mem address of the value recieved
  * Description: Return the converted number, or 0 if,
- * there is one or more chars in the string b that is not 0 or 1,
+ * there is one or more chars in the string b that is not 0 or 1 or
  * b is NULL
  *
  * Return: An unsigned value

33d63cf339df4bff4e3d4c49801c6d4227ec418a
MucheruM <petermucheru420@gmail.com>
2023-10-26 17:58:34 -0700

    Write a function that returns the number of bits you would need to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,13 @@
-#iclude "main.h"
+#include "main.h"
 
 /**
  * flip_bits - Return the num of bits needed to flip
  * @n: first num of bits
  * @m: second num of bits
+ *
  * Return: Value of bit at index, or -1 if error
  */
-int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int XOR = n ^ m, bits = 0;
 

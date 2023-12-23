a3157a120017c29fad9ac40ddd5667546c920d0e
MucheruM <petermucheru420@gmail.com>
2023-12-22 16:29:11 -0800

    Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
+	{
+		free (hash_t);
 		return (NULL);
+	}
 
 	hash_t->size = size;
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t->array == NULL)
+	{
+		free (hash_t->array);
 		return (NULL);
+	}
+
 	for (j = 0; j < size; j++)
 		hash_t->array[j] = NULL;
 

b457723980a3b231edb2ea8a598e05abe0a4ea76
MucheruM <petermucheru420@gmail.com>
2023-12-22 16:33:37 -0800

    Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,16 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
-	{
 		return (NULL);
-	}
 
 	hash_t->size = size;
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t->array == NULL)
-	{
 		return (NULL);
-	}
 
 	for (j = 0; j < size; j++)
 		hash_t->array[j] = NULL;
